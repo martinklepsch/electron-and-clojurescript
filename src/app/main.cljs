960e81863e20b611e6909b6b68844e1cc15a4b25
Yury <YurySolovyov@users.noreply.github.com>
2016-09-05 19:17:37 +0300

    Fix to work with latest electron (#1)

--- src/app/main.cljs
+++ src/app/main.cljs
@@ -1,12 +1,13 @@
-(ns app.main) 
+(ns app.main)
 
-(def app           (js/require "app"))
-(def BrowserWindow (js/require "browser-window"))
+(def electron      (js/require "electron"))
+(def app           (.-app electron))
+(def BrowserWindow (.-BrowserWindow electron))
 
 (goog-define dev? false)
 
 (defn load-page
-  "When compiling with `:none` the compiled JS that calls .loadUrl is
+  "When compiling with `:none` the compiled JS that calls .loadURL is
   in a different place than it would be when compiling with optimizations
   that produce a single artifact (`:whitespace, :simple, :advanced`).
 
@@ -15,8 +16,8 @@
   at compile time using the `:clojure-defines` compiler option."
   [window]
   (if dev?
-      (.loadUrl window (str "file://" js/__dirname "/../../index.html"))
-      (.loadUrl window (str "file://" js/__dirname "/index.html"))))
+      (.loadURL window (str "file://" js/__dirname "/../../index.html"))
+      (.loadURL window (str "file://" js/__dirname "/index.html"))))
 
 (def main-window (atom nil))
 
